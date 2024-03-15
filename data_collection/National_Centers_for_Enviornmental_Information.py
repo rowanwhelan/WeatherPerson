516c2f3e59e1492165cb638d616f335cb54fb353
Rowan Quinn Whelan <rWhelan340@gmail.com>
2024-03-15 18:59:11 -0400

    first half of NCEI

--- data_collection/National_Centers_for_Enviornmental_Information.py
+++ data_collection/National_Centers_for_Enviornmental_Information.py
@@ -1,30 +1,63 @@
+import numpy as np
 import requests,sys 
 sys.path.append("../")
-from weather_cnn.keys.NCEI import key
+from CS_546.keys.NCEI import key
 
-def collect_NCEI_data(latitude, longitude):
-    start_date = '2022-01-01'
-    end_date = '2022-01-31'
-    location = str(latitude) + ',' + str(longitude)
-    data_set = 'global-historical-climatology-network-daily'
-    
-    api_url = f'https://www.ncei.noaa.gov/access/services/data/v1'
-    
-    params = {
-        'dataset': data_set,
-        'startDate': start_date,
-        'endDate': end_date,
-        'location': location,
-        'format': 'csv',
-        'api-key': key
-    }
-    
-    response = requests.get(api_url, params=params)
-    
+def tabulate_response(response,data,value):
     if response.status_code == 200:
-        data = response.text
-        print(data)
+            data = response.json()
+            for observation in data['results']:
+                date = observation['date']    
+                value = observation['value']
+                
+                data[index_of_day(date),] = value
     else:
         print(f'Error: {response.status_code} - {response.text}')
 
-collect_NCEI_data(40.7794,-73.9691)
+def collect_NCEI_data():
+    data = np.array([[],[],[],[]])
+    for i in range(1980, 2023):
+        start_date = str(i)+'-01-01'
+        end_date = str(i)+'-12-31'
+        data_set = 'GHCND'
+
+        api_url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
+        params = {
+            'datasetid': data_set,
+            'startdate': start_date,
+            'enddate': end_date,
+            'stationid': 'GHCND:USW00014732',
+            'format': 'csv',
+            'limit': '365',
+            'datatypeid': ['ACMH']
+        }
+        headers = {'token': key}
+        response = requests.get(api_url, params=params, headers=headers)
+
+        params = {
+            'datasetid': data_set,
+            'startdate': start_date,
+            'enddate': end_date,
+            'stationid': 'GHCND:USW00014732',
+            'format': 'csv',
+            'limit': '365',
+            'datatypeid': ['ACMH']
+        }
+        
+        response = requests.get(api_url, params=params, headers=headers)
+        
+        params = {
+            'datasetid': data_set,
+            'startdate': start_date,
+            'enddate': end_date,
+            'stationid': 'GHCND:USW00014732',
+            'format': 'csv',
+            'limit': '366',
+            'datatypeid': ['TMAX']
+        }
+        
+        response = requests.get(api_url, params=params, headers=headers)
+            
+        
+
+collect_NCEI_data()
---
+++ data_collection/National_Weather_Service.py
@@ -0,0 +1,23 @@
+import requests,sys 
+sys.path.append("../")
+
+def collect_NCEI_data():
+    #Belvedere
+    stationid = 'KW035'
+    start = '1980-01-01T00:00:00+19:00'
+    end = '2023-12-21T24:00:00+19:00'
+    api_url = f'https://api.weather.gov/stations/{start, end, stationid}/observations'
+    response = requests.get(api_url, headers= {'User-Agent': 'WeatherPredictorApp/1.0 contact rwhelan340@gmail.com', 'Accept': 'application/json'})
+
+    
+    # Check if request was successful
+    if response.status_code == 200:
+    # Parse the JSON response
+        data = response.json()
+    # Print the entire response for inspection
+        print(data)
+    else:
+        print('Error:', response.status_code)
+        print('Response Content:', response.text)  # Print response content for inspection
+
+collect_NCEI_data()
