7bd5930ff31a48269bab0c4b22028db5131381d3
Rowan Quinn Whelan <rWhelan340@gmail.com>
2024-03-15 19:43:32 -0400

    2/3 NCEI

--- data_collection/National_Centers_for_Enviornmental_Information.py
+++ data_collection/National_Centers_for_Enviornmental_Information.py
@@ -1,16 +1,34 @@
+from datetime import datetime
+import math
 import numpy as np
 import requests,sys 
 sys.path.append("../")
 from CS_546.keys.NCEI import key
 
-def tabulate_response(response,data,value):
+def index_of_day(date_str):
+    date_str = date_str[0:10]
+    try:
+        # Parse the date string into a datetime object
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        # Get the day of the year
+        day_of_year = date_obj.timetuple().tm_yday
+        year = int(date_str[0:4])
+        day_of_year = day_of_year + ((-1980 + year)*365 + math.floor((-1980 + year) / 4))
+        print(day_of_year)
+        return day_of_year
+    except ValueError:
+        # Handle invalid date string
+        print("Invalid date format. Please provide date in YYYY-MM-DD format.")
+
+def tabulate_response(response,data,datatype):
     if response.status_code == 200:
-            data = response.json()
-            for observation in data['results']:
-                date = observation['date']    
-                value = observation['value']
+        data = response.json()
+        for observation in data['results']:
+            date = observation['date']    
+            value = observation['value']
                 
-                data[index_of_day(date),] = value
+            data[index_of_day(date),datatype] = value
+        return data
     else:
         print(f'Error: {response.status_code} - {response.text}')
 
@@ -28,24 +46,23 @@ def collect_NCEI_data():
             'enddate': end_date,
             'stationid': 'GHCND:USW00014732',
             'format': 'csv',
-            'limit': '365',
+            'limit': '366',
             'datatypeid': ['ACMH']
         }
         headers = {'token': key}
         response = requests.get(api_url, params=params, headers=headers)
-
+        data.append = tabulate_response(response, data, 1)
         params = {
             'datasetid': data_set,
             'startdate': start_date,
             'enddate': end_date,
             'stationid': 'GHCND:USW00014732',
             'format': 'csv',
-            'limit': '365',
+            'limit': '366',
             'datatypeid': ['ACMH']
         }
-        
         response = requests.get(api_url, params=params, headers=headers)
-        
+        tabulate_response(response, data, 2)
         params = {
             'datasetid': data_set,
             'startdate': start_date,
@@ -55,9 +72,7 @@ def collect_NCEI_data():
             'limit': '366',
             'datatypeid': ['TMAX']
         }
-        
         response = requests.get(api_url, params=params, headers=headers)
-            
-        
-
-collect_NCEI_data()
+        tabulate_response(response, data, 2)
+index_of_day('1986-12-04')
+index_of_day('1980-01-04')
